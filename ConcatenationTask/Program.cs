b6db9e19c512b16ec5adc322950c1d65899b8ee4
Al Stone <astone@heckgrammar.co.uk>
2024-06-28 09:42:59 +0100

    Add project files.

---
+++ ConcatenationTask/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConcatenationTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName;
+            Console.Write("Enter first name: ");
+            FirstName = Console.ReadLine();
+            // add two more variables for the users last name and their age
+            // take these inputs into the program
+            // use the test data Bob, Smith, 14
+            // ammend this code to output in one command
+            // Hello Bob Smith you are 14 years old
+            Console.WriteLine(FirstName);
+            // save and commit your program
+            // add evidence of the test input and your own input running the program
+        }
+    }
+}
